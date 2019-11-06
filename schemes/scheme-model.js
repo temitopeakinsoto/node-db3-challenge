f1aff721996ff8abae1ab0608082b9d3115a75f6
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-06 22:51:29 +0100

    Create add() helper function
    In this commit, I created the add(schemedata) helper function for adding a new scheme to the database. This function expects a scheme data object which it then inserts into the database

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,7 +3,8 @@ const db = require('../data/db-config');
 module.exports = {
   find,
   findById,
-  findSteps
+  findSteps,
+  add
 };
 
 function find() {
@@ -18,7 +19,15 @@ function findById(schemeId){
 }
 
 function findSteps(schemeId){
-    return db('steps')   
-    .select('steps.id', 'steps.instructions')
-    .where({ 'steps.scheme_id': schemeId });
+    return db('steps as s') 
+    .join('schemes', 'schemes.id', 's.scheme_id')  
+    .select('s.id', 's.instructions', 'scheme_name')
+    .where({ 's.scheme_id': schemeId });
+}
+
+function add(schemeData){
+    const schemeToBeCreated = { scheme_name : schemeData.scheme_name}
+    return db("schemes")
+    .insert(schemeToBeCreated);
+    
 }
