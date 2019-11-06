1845ef918aea2c24bc341531dcac9098aca1baad
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-06 22:31:45 +0100

    Add the findSteps(id) helper function
    In this commit, I created the findStesp(id) function. this function expects a scheme id and returns all the steps tied to that particular scheme id

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,7 +2,8 @@ const db = require('../data/db-config');
 
 module.exports = {
   find,
-  findById
+  findById,
+  findSteps
 };
 
 function find() {
@@ -14,4 +15,10 @@ function findById(schemeId){
     return db('schemes as s')
     .select('s.id', 's.scheme_name')
     .where({ 's.id': schemeId });
+}
+
+function findSteps(schemeId){
+    return db('steps')   
+    .select('steps.id', 'steps.instructions')
+    .where({ 'steps.scheme_id': schemeId });
 }
