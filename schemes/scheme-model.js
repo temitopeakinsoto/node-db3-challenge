a1e65e9192d8280919ba2409de28be840108d26a
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-06 21:55:43 +0100

    Add the findById() helper function
    In this commit, I created the findById() helper function which when
    invoked, returns a particular scheme. The findById takes a single idparameter which is used to query the schemes database

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,9 +2,16 @@ const db = require('../data/db-config');
 
 module.exports = {
   find,
+  findById
 };
 
 function find() {
     return db('schemes as s')
     .select('s.id', 's.scheme_name');
+}
+
+function findById(schemeId){
+    return db('schemes as s')
+    .select('s.id', 's.scheme_name')
+    .where({ 's.id': schemeId });
 }
